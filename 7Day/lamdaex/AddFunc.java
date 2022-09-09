e250401053992a63cf5a71e2f2bb38c575f7a067
UdithDevadiga <111740361+UdithDevadiga@users.noreply.github.com>
2022-09-09 16:38:53 +0530

    Delete 7Day/lamdaex directory

--- 7Day/lamdaex/AddFunc.java
+++
@@ -1,6 +0,0 @@
-package lamdaex;
-@FunctionalInterface
-public interface AddFunc {
-	int add(int a,int b);
-
-}
--- 7Day/lamdaex/Main.java
+++
@@ -1,7 +0,0 @@
-package lamdaex;
-class Main{
-	public static void main(String[] args) {
-	AddFunc a1=(a,b)->a+b;
-	System.out.println(a1.add(10, 20));
-	}
-}
