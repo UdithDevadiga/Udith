1664d4153ca1aa02068bf0c2990a5b5f48a60872
UdithDevadiga <111740361+UdithDevadiga@users.noreply.github.com>
2022-09-09 16:39:13 +0530

    Delete 9Day directory

--- 9Day/practice2/Employee.java
+++
@@ -1,8 +0,0 @@
-package practice2;
-public class Employee extends Member {
-	Employee(String name, int age, String phoneNo, String address, double salary) {
-		super(name, age, phoneNo, address, salary);
-	}
-	String specialization;
-	String department;
-}
--- 9Day/practice2/Manager.java
+++
@@ -1,8 +0,0 @@
-package practice2;
-public class Manager extends Member{
-	Manager(String name, int age, String phoneNo, String address, double salary) {
-		super(name, age, phoneNo, address, salary);
-	}
-	String specialization;
-	String department;
-}
