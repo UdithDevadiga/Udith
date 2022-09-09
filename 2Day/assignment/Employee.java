f4dafddb273bbdafa876a79554fe5856b9b47005
UdithDevadiga <111740361+UdithDevadiga@users.noreply.github.com>
2022-09-09 16:37:42 +0530

    Delete 2Day directory

--- 2Day/assignment/Employee.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-
-public class Employee extends Member {
-	Employee(String name, int age, String phoneNo, String address, double salary) {
-		super(name, age, phoneNo, address, salary);
-	}
-	String specialization;
-	String department;
-}
--- 2Day/assignment/Manager.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-
-public class Manager extends Member{
-	Manager(String name, int age, String phoneNo, String address, double salary) {
-		super(name, age, phoneNo, address, salary);
-	}
-	String specialization;
-	String department;
-}
