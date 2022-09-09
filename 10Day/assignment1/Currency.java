8bf52c065288e080d9685fc74815c4b7458947a9
UdithDevadiga <111740361+UdithDevadiga@users.noreply.github.com>
2022-09-09 16:37:19 +0530

    Delete 10Day directory

--- 10Day/assignment1/Currency.java
+++
@@ -1,7 +0,0 @@
-package assignment1;
-
-public abstract class Currency {
-	abstract String getCurrency();
-	abstract String getSymbol();
-
-}
--- 10Day/assignment2/CarType.java
+++
@@ -1,7 +0,0 @@
-package assignment2;
-
-public enum CarType {
-	SMALL,
-	SEDAN,
-	LUXURY
-}
